695c6b98df8f15059233ae4d9038d76d9b16ee5d
BaraKex <barzenkov.alexey@yandex.ru>
2018-08-07 17:54:55 +0300

    person+monster

--- src/App/containers/Dashboard/data.js
+++ src/App/containers/Dashboard/data.js
@@ -1,79 +1,124 @@
 const data = {
-  walls: [
+  cells: [
     {
       id: 1,
       x: 7,
       y: 3,
+      type: 'wall',
     },
     {
       id: 2,
       x: 7,
       y: 4,
+      type: 'wall',
     },
     {
       id: 3,
       x: 7,
       y: 5,
+      type: 'wall',
     },
     {
       id: 4,
       x: 7,
       y: 6,
+      type: 'wall',
     },
     {
       id: 5,
       x: 7,
       y: 7,
+      type: 'wall',
     },
     {
       id: 6,
       x: 7,
       y: 8,
+      type: 'wall',
     },
     {
       id: 7,
       x: 11,
       y: 4,
+      type: 'wall',
     },
     {
       id: 8,
       x: 11,
       y: 5,
+      type: 'wall',
     },
     {
       id: 9,
       x: 11,
       y: 6,
+      type: 'wall',
     },
     {
       id: 10,
       x: 11,
       y: 7,
+      type: 'wall',
     },
     {
       id: 11,
       x: 11,
       y: 8,
+      type: 'wall',
     },
     {
       id: 12,
       x: 8,
       y: 3,
+      type: 'wall',
     },
     {
       id: 13,
       x: 9,
       y: 3,
+      type: 'wall',
     },
     {
       id: 14,
       x: 10,
       y: 3,
+      type: 'wall',
     },
     {
       id: 15,
       x: 11,
       y: 3,
+      type: 'wall',
+    },
+    {
+      id: 15,
+      x: 9,
+      y: 7,
+      type: 'cell',
+      content: {
+        person: {
+          id: 1,
+          name: 'slon',
+          type: 'monstr',
+        },
+        subject: {
+          name: 'arrow of dastiny',
+        },
+      },
+    },
+    {
+      id: 16,
+      x: 9,
+      y: 5,
+      type: 'cell',
+      content: {
+        person: {
+          id: 1,
+          name: 'slon',
+          type: 'player',
+        },
+        subject: null,
+      },
     },
   ],
 };
