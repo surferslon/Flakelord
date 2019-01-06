2c198ae6a03ddb38d72b0c433dced664d514ffd2
surferslon <elefant400@gmail.com>
2019-01-06 19:06:09 +0300

    Graphics and transparency

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 window.onload = function() {
 
-    var stage = document.getElementById('cnvs')
+    var stage       = document.getElementById('cnvs')
     var ctx         = stage.getContext('2d')
     var ctx_titles  = stage.getContext('2d')
     var ctx_text    = stage.getContext('2d')
 
     var map = [
-    '#.................................###############..........#',
-    '..................................#.............#...........',
-    '..................................#..########...#...........',
-    '......######...........############..#......#...#...........',
-    '......#....#...........#.............#......#####...########',
-    '......#....#...........#.####..#######.........######......#',
-    '......#....#############....#..#################...........#',
-    '......#..................####....................###########',
-    '......############.#######..######################..........',
-    '.................#.#........................................',
-    '.................#.#........................................',
-    '.................#.#........................................',
-    '.................#.#........................................',
-    '.................#.#############.........############.......',
-    '.................#.............#.........#..........#.......',
-    '.................#.............#.........#.########.#.......',
-    '.................#.............#.........#.#......#.#.......',
-    '................##.............#.........#.#......#.#.......',
-    '................#...#########..###########.#......#.#.......',
-    '................#...#.......#..............#......#.#.......',
-    '................#...#.......################......#.#.......',
-    '................#...#.............................#.#.......',
-    '#################...#....##########...............#.#.......',
-    '#...................#....#........#...............#.#.......',
-    '#..##################....#........#....############.######..',
-    '#..#.....#...............#........#....#.................#..',
-    '#..#.....#################........#....#.................#..',
-    '#.................................#....###################..',
-    '####################..............#.........................',
-    '#..................################........................#'
+    '                                  ###############          #',
+    '                                  #.............#           ',
+    '                                  #..########...#           ',
+    '      ######           ############..#......#...#           ',
+    '      #....#           #.............#......#####   ########',
+    '      #....#           #.####..#######.........######......#',
+    '      #....#############....#..#################...........#',
+    '      #..................####....................###########',
+    '      ############.#######  ######################          ',
+    '                 #.#                                        ',
+    '                 #.#                                        ',
+    '                 #.#                                        ',
+    '                 #.#                                        ',
+    '                 #.#############         ############       ',
+    '                 #.............#         #..........#       ',
+    '                 #.............#         #.########.#       ',
+    '                 #.............#         #.#      #.#       ',
+    '                ##.............#         #.#      #.#       ',
+    '                #...#########..###########.#      #.#       ',
+    '                #...#       #..............#      #.#       ',
+    '                #...#       ################      #.#       ',
+    '                #...#                             #.#       ',
+    '#################...#    ##########               #.#       ',
+    '#...................#    #........#               #.#       ',
+    '#..##################    #........#    ############.######  ',
+    '#..#.....#               #........#    #.................#  ',
+    '#..#.....#################........#    #.................#  ',
+    '#.................................#    ###################  ',
+    '####################..............#                         ',
+    '#                  ################                        #'
     ]
 
     var cell_height = 20;
@@ -49,6 +49,13 @@ window.onload = function() {
         wid = 20, 
         hei = 30; 
 
+    var img_player = new Image();
+    img_player.src = 'rsz_knight.png';
+    var img_wall = new Image();
+    img_wall.src = 'wall.png';
+    var img_floor = new Image();
+    img_floor.src = 'floor.png';
+
     stage.width  = stage.offsetWidth;
     stage.height = stage.offsetHeight;
     
@@ -60,20 +67,9 @@ window.onload = function() {
     }
 
     function drawPlayer(x, y, wid, hei) {
-        var img = new Image();
-        img.src = 'rsz_knight.png';
-        var player_img_x = 600 + ((x - y) * cell_width/2);
-        var player_img_y = (y + x) * cell_height/2;
-        img.onload = function() {
-            var pattern = ctx.createPattern(img, '');
-            //ctx.fillRect(x, y, wid, hei);
-            
-            ctx.drawImage(img, player_img_x-cell_width/4, player_img_y-cell_height+3);
-
-            // ctx_titles.beginPath();
-            // ctx_titles.drawImage(img, 200, 100);
-        };
-        // drawRhomb(player_img_x, player_img_y);
+        coords = ConvertToCoord(x, y)
+        ctx.drawImage(img_player, coords.x-cell_width/4, coords.y-cell_height+3);
+        // drawRhomb(coords.x, coords.y);
     }
 
     function drawRhomb(x, y) {
@@ -83,43 +79,73 @@ window.onload = function() {
         ctx.lineTo(x, y + cell_height);
         ctx.lineTo(x - cell_width / 2, y + cell_height / 2);
         ctx.lineTo(x, y);
-        ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+        // ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+        ctx.strokeStyle = 'white';
         ctx.stroke();
-
-        var img = new Image();
-        img.src = 'wall.png';
-        img.onload = function() {
-            var pattern = ctx.createPattern(img, '');
-            ctx.beginPath();
-            ctx.drawImage(img, x, y);
-            ctx_titles.drawImage(img, x, y);
-        }
-
         ctx.closePath();
     }
 
-    function drawMap() {
+    function drawWall(x, y) {
+        ctx.drawImage(img_wall, x-22, y-cell_height-16);
+    }
+
+    function drawFloor(x, y) {
+        ctx.drawImage(img_floor, x-23, y);
+    }
+
+    function drawMap_background() {
+
+        // Floor
         for(var cur_clm=0; cur_clm<61; cur_clm++) {
             for(var cur_row=0; cur_row<30; cur_row++) {
 
                 let cell_x = (600 + cur_clm * cell_width / 2) - cur_row * cell_width / 2;
                 let cell_y = cur_clm * cell_height / 2 + cur_row * cell_height / 2;
 
-                if (map[cur_row][cur_clm] == '#') {
+                if (map[cur_row][cur_clm] == '.') {
                     coord = ConvertToCoord(cur_clm, cur_row);
-                    drawRhomb(coord.x, coord.y, 255); 
-                }
-                else {
-                    // drawRhomb(cell_x, cell_y, 0); 
+                    drawFloor(coord.x, coord.y); 
                 }
             }
         }
+
+        // Background walls
+        for(var cur_clm=0; cur_clm<61; cur_clm++) {
+            for(var cur_row=0; cur_row<30; cur_row++) {
+                if (map[cur_row][cur_clm] == '#') {
+                    coord = ConvertToCoord(cur_clm, cur_row);
+                    // if ( Math.abs(cur_row - player_y) < 4 && Math.abs(cur_clm - player_x) < 4 ) {
+                    let diff_x = cur_clm - player_x;
+                    let diff_y = cur_row - player_y;
+                    if (cur_clm >= player_x && cur_row >= player_y && diff_y < 2 && diff_x<2) {
+                        ctx.globalAlpha = 0.1;
+                    }
+                    else if (cur_clm >= player_x && cur_row >= player_y && diff_y < 4 && diff_x<4) {
+                        ctx.globalAlpha = 0.3;
+                    }
+                    else if (cur_clm >= player_x && cur_row >= player_y && diff_y < 6 && diff_x<6) {
+                        ctx.globalAlpha = 0.6;
+                    }
+                    else if (cur_clm >= player_x && cur_row >= player_y && diff_y < 7 && diff_x<7) {
+                        ctx.globalAlpha = 0.8;
+                    }
+                    drawWall(coord.x, coord.y); 
+                    ctx.globalAlpha = 1;
+                }
+                if (cur_row==player_y && cur_clm==player_x) {   
+                    drawPlayer(player_x, player_y, wid, hei);
+                }
+            }
+        }
+
     }
 
-    drawPlayer(player_x, player_y, wid, hei)
 
-    drawMap()
-
+    function drawMap() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, stage.width, stage.height);
+        drawMap_background();
+    }
 
     function collision(x, y) {
         if (map[y][x] != '.') {
@@ -164,8 +190,8 @@ window.onload = function() {
         
         ctx.clearRect(0, 0, 1800, 900); //clearing anything drawn on canvas
       
-        drawPlayer(player_x, player_y, wid, hei)
         drawMap()
+
     }
 
 
@@ -180,18 +206,19 @@ window.onload = function() {
 
         ctx.clearRect(0, 0, 1800, 900); //clearing anything drawn on canvas
 
-        drawPlayer(player_x, player_y, wid, hei)
         drawMap()
 
         coord = ConvertToCoord(tileX, tileY);
         drawRhomb(coord.x, coord.y);
 
-        ctx.font = "15px Arial";
-        if (tileY < 30 && tileY > 0 && tileX < 60 && tileX > 0 && map[tileY][tileX] == '#') {
-            ctx.fillText("Wall", coord.x, coord.y); 
+        ctx.font = "15px Courier";
+        ctx.fillStyle = 'white';
+
+        if (tileY < 30 && tileY >= 0 && tileX <= 60 && tileX >= 0 && map[tileY][tileX] == '#') {
+            ctx.fillText("Wall", coord.x+20, coord.y);
         }
         else if (tileY == player_y && tileX == player_x) {
-            ctx.fillText("Player", coord.x+20, coord.y); 
+            ctx.fillText("Player", coord.x+20, coord.y);
         }
 
       }, false);
