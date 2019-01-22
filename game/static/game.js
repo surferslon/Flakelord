2c69398a35829573ba93ac3a251c978b2486ff5b
surferslon <elefant400@gmail.com>
2019-01-23 00:04:12 +0300

    Centered

--- game/static/game.js
+++ game/static/game.js
@@ -38,15 +38,19 @@ window.onload = function() {
 ]
 
 
-    var cell_height = 20;
-    var cell_width = 40;
+    var cell_height = 40;
+    var cell_width = 60;
+
 
     var player_x = 8;
     var player_y = 5;
     var player_dir = 'r';
+
+    var draw_start_x = 900 - (player_x) * cell_width/4;
+    var draw_start_y = 450 - (player_y) * cell_height;
     
-    var y = 5*cell_height,
-        x = (600 - 8*cell_width) + 5*cell_width, // Start positions 
+    var y = draw_start_y + 5*cell_height,
+        x = (draw_start_x - 8*cell_width) + 5*cell_width, // Start positions 
         wid = 20, 
         hei = 30; 
 
@@ -69,26 +73,25 @@ window.onload = function() {
     img_monster_r.src = '/static/img/ork.png'
     img_monster_r.src = '/static/img/ork.png'
 
-
     stage.width  = stage.offsetWidth;
     stage.height = stage.offsetHeight;
     
     function ConvertToCoord(x, y) {
         return {
-            x: 600 + ((x - y) * cell_width/2),
-            y: (y + x) * cell_height/2
+            x: draw_start_x + ((x - y) * cell_width/2),
+            y: draw_start_y + (y + x) * cell_height/2
         }
     }
 
     function drawPlayer(x, y, wid, hei) {
         coords = ConvertToCoord(x, y)
+        drawRhomb(coords.x, coords.y);
         if (player_dir=='r'){
-            ctx.drawImage(img_player_r, coords.x-cell_width/4, coords.y-cell_height+3);
+            ctx.drawImage(img_player_r, coords.x-cell_width/4, coords.y-cell_height+13, 35, 50);
         }
         else {
-            ctx.drawImage(img_player_l, coords.x-cell_width/4, coords.y-cell_height+3);   
+            ctx.drawImage(img_player_l, coords.x-cell_width/4, coords.y-cell_height+13, 35, 50);
         }
-        // drawRhomb(coords.x, coords.y);
     }
 
     function drawMonster(x, y, wid, hei) {
@@ -115,11 +118,11 @@ window.onload = function() {
     }
 
     function drawWall(x, y) {
-        ctx.drawImage(img_wall, x-22, y-cell_height-16);
+        ctx.drawImage(img_wall, x-22, y-cell_height-16, cell_width+5, cell_height+57);
     }
 
     function drawFloor(x, y) {
-        ctx.drawImage(img_floor, x-23, y);
+        ctx.drawImage(img_floor, x-23, y, cell_width, cell_height);
     }
 
     function drawMap_background() {
@@ -128,8 +131,8 @@ window.onload = function() {
         for(var cur_clm=0; cur_clm<60; cur_clm++) {
             for(var cur_row=0; cur_row<30; cur_row++) {
 
-                let cell_x = (600 + cur_clm * cell_width / 2) - cur_row * cell_width / 2;
-                let cell_y = cur_clm * cell_height / 2 + cur_row * cell_height / 2;
+                // let cell_x = (600 + cur_clm * cell_width / 2) - cur_row * cell_width / 2;
+                // let cell_y = cur_clm * cell_height / 2 + cur_row * cell_height / 2;
 
                 if (map[cur_row][cur_clm]['obj'] == 'floor') {
                     coord = ConvertToCoord(cur_clm, cur_row);
@@ -201,35 +204,47 @@ window.onload = function() {
             player_x = player_x+1;
             player_y = player_y-1;
             player_dir = 'r';
+            draw_start_x = draw_start_x-cell_width;
         } // right
         else if(key === 69 && x>0 && !collision(player_x, player_y-1)) { 
             player_y = player_y-1;
             player_dir = 'r';
+            draw_start_x = draw_start_x-cell_width/2;
+            draw_start_y = draw_start_y+cell_height/2;
         } // right up
         else if(key === 67 && x>0 && !collision(player_x+1, player_y)) { 
             player_x = player_x+1; 
             player_dir = 'r';
+            draw_start_x = draw_start_x-cell_width/2;
+            draw_start_y = draw_start_y-cell_height/2;
         } // rigth down
         else if(key === 65 && x>0 && !collision(player_x-1, player_y+1)) { 
             player_x = player_x-1; 
             player_y = player_y+1;
             player_dir = 'l';
+            draw_start_x = draw_start_x+cell_width;
         } // left
         else if(key === 81 && player_x>0 && !collision(player_x-1, player_y)) {
             player_x = player_x-1;
             player_dir = 'l';
+            draw_start_x = draw_start_x+cell_width/2;
+            draw_start_y = draw_start_y+cell_height/2;
         } // left up
         else if(key === 87 && y>0 && !collision(player_x-1, player_y-1)) { 
             player_x = player_x-1;
             player_y = player_y-1;
+            draw_start_y = draw_start_y+cell_height;
         } // up
         else if(key === 88 && y<=600 && !collision(player_x+1, player_y+1)) {
             player_x = player_x+1;
             player_y = player_y+1; 
+            draw_start_y = draw_start_y-cell_height;
         } // down
         else if(key === 90 && player_x>0 && !collision(player_x, player_y+1)) {
             player_y = player_y+1; 
             player_dir = 'l';
+            draw_start_x = draw_start_x+cell_width/2;
+            draw_start_y = draw_start_y-cell_height/2;
         } // left down
         
         ctx.clearRect(0, 0, 1800, 900); //clearing anything drawn on canvas
@@ -254,8 +269,8 @@ window.onload = function() {
         x = evnt.clientX - rect.left;
         y = evnt.clientY - rect.top;
 
-        tileX = Math.round((x - 600 ) / cell_width + y / cell_height);
-        tileY = Math.round(y / cell_height - (x-600) / cell_width);
+        tileX = Math.round((x - draw_start_x ) / cell_width + y / cell_height);
+        tileY = Math.round(y / cell_height - (x-draw_start_x) / cell_width);
 
         ctx.clearRect(0, 0, 1800, 900); //clearing anything drawn on canvas
 
